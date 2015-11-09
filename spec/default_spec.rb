29956843d2057d8a89f01d62ebf6a5ef5aa110ce
luke bradbury <luke.dolman@gmail.com>
2015-11-09 20:51:09 +0000

    add roundrobin loadbalancing with tests. minor spec_helper refactor

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,6 +1,22 @@
 # spec/example_spec.rb
 require 'spec_helper'
 
+describe server(:app1) do
+  describe http('http://app:8484') do
+    it "responds content including #{APP1_STRING}" do
+      expect(response.body).to include(APP1_STRING)
+    end
+  end
+end
+
+describe server(:app2) do
+  describe http('http://app:8484') do
+    it "responds content including #{APP2_STRING}" do
+      expect(response.body).to include(APP2_STRING)
+    end
+  end
+end
+
 describe server(:web) do
   describe http('http://app') do
     it "responds content including 'Hi there, I'm served from'" do
@@ -8,17 +24,9 @@ describe server(:web) do
     end
   end
 end
-describe server(:app1) do
-  describe http('http://app:8484') do
-    it "responds content including 'Hi there, I'm served from default-thrashing-volcano-2002!'" do
-      expect(response.body).to include('Hi there, I\'m served from default-thrashing-volcano-2002!')
-    end
-  end
-end
-describe server(:app2) do
-  describe http('http://app:8484') do
-    it "responds content including 'Hi there, I'm served from default-pensive-landscape-4090!'" do
-      expect(response.body).to include('Hi there, I\'m served from default-pensive-landscape-4090!')
-    end
-  end
+
+describe 'round_robin responses should be balanced' do
+  subject { round_robin }
+  it { is_expected.to satisfy("respond 5 times with " + APP1_STRING) { |r| r.count(APP1_STRING) == 5 } }
+  it { is_expected.to satisfy("respond 5 times with " + APP2_STRING) { |r| r.count(APP2_STRING) == 5 } }
 end
