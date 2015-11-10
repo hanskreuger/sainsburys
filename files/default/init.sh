c25373b5da25015a889e41740fb04ae66b907c2c
luke bradbury <luke.dolman@gmail.com>
2015-11-10 10:24:24 +0000

    improved init script - adds status command, so doesn't restart service on every converge

--- files/default/init.sh
+++ files/default/init.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
-
-# sainsburys
 #
-# chkconfig:   - 57 47
-# description: sainsburys
-# processname:  sainsburys
+# chkconfig: 35 90 12
+# description: sainsburys go server
 #
+# Get function from functions library
+. /etc/init.d/functions
 
-case $1 in
-    start)
-        echo "Starting sainsburys web app."
+start() {
+        initlog -c "echo -n Starting sainsburys server: "
         /opt/go/bin/sainsburys &
+        ### Create the lock file ###
+        touch /var/lock/subsys/sainsburys
+        success $"sainsburys server startup"
+        echo
+}
+stop() {
+        initlog -c "echo -n Stopping sainsburys server: "
+        killproc sainsburys
+        ### Now, delete the lock file ###
+        rm -f /var/lock/subsys/sainsburys
+        echo
+}
+### main logic ###
+case "$1" in
+  start)
+        start
         ;;
-    stop)
-        echo "Stopping sainsburys web app."
-        sudo kill $(sudo lsof -t -i:8484)
+  stop)
+        stop
         ;;
-    *)
-        echo "sainsburys web app service."
-        echo $"Usage $0 {start|stop}"
+  status)
+        status sainsburys
+        ;;
+  restart)
+        stop
+        start
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart|status}"
         exit 1
 esac
 exit 0
